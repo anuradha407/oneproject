c238d80348aab00a5d0bc2c1b6dd951472b649c1
papa <anutony23@gmail.com>
2021-03-13 19:10:59 +0530

    dffdf

--- jobs/models.py
+++ jobs/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Job(models.Model):
     image=models.ImageField(upload_to='images/')
     summary= models.CharField(max_length=300)
+class Post(models.Model):
+    first_name= models.CharField(max_length=300)
+    last_name = models.CharField(max_length=300)
